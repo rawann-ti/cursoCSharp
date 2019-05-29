fe7943819d2058c3fe906531b90d66608e52578d
Rawann Silva <rawannsilva.ti@gmail.com>
2019-05-28 22:09:10 -0300

    Acrescentada uma mensagem de bom dia

--- testegit/Program.cs
+++ testegit/Program.cs
@@ -7,6 +7,7 @@ namespace testegit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa noite");
         }
     }
 }
